cd7943199a4f1e48b9cf6349079e6876ed816fa9
Dubois-Pèlerin Yves (DIN) <yves.dubois-pelerin@etat.ge.ch>
2025-05-19 12:21:05 +0200

    Clean up main class Application

--- src/main/java/ch/ge/cti/nexus/nexusrmgui/Application.java
+++ src/main/java/ch/ge/cti/nexus/nexusrmgui/Application.java
@@ -17,67 +17,69 @@ package ch.ge.cti.nexus.nexusrmgui;
 
 import ch.ge.cti.nexus.nexusrmgui.service.CertificateService;
 import ch.ge.cti.nexus.nexusrmgui.service.ComponentService;
-
 import ch.ge.cti.nexus.nexusrmgui.service.PermissionService;
 import jakarta.annotation.Resource;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
+import static java.util.Locale.ENGLISH;
 
 @SpringBootApplication
 @Slf4j
 public class Application implements CommandLineRunner {
 
-    int days;
+    @Resource
+    private CertificateService certificateService;
 
     @Resource
-    ApplicationContext applicationContext;
+    private ComponentService componentService;
 
-    public static void main(String[] args) throws Exception {
+    @Resource
+    private PermissionService permissionService;
+
+    int days;
+
+    public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     public void run(String... args) throws InterruptedException {
+        final int DAYS_IN_MONTH = 30;
+
         if (args.length > 0) {
             String option = args[0];
 
             switch (option) {
                 case "certificates", "1":
-                    var certificateService = applicationContext.getBean(CertificateService.class);
                     if (args.length > 1) {
                         days = Integer.parseInt(args[1]);
                     } else {
-                        days = 30;
+                        days = DAYS_IN_MONTH;
                     }
                     certificateService.showExpiredCertificates(days);
                     break;
                 case "heavyComponents", "2":
-                    var componentService = applicationContext.getBean(ComponentService.class);
                     componentService.showComponents();
                     break;
                 case "permissions", "3":
-                    var roleService = applicationContext.getBean(PermissionService.class);
                     if (args.length > 1) {
-                        var userId = args[1].toUpperCase();
-                        roleService.showPermissions(userId);
+                        var userId = args[1].toUpperCase(ENGLISH);
+                        permissionService.showPermissions(userId);
                     } else {
                         System.out.println("Please provide a user ID for the 'permission' option.");
                     }
                     break;
                 case "deleteComponents", "4":
-                    var componentServiceForDelete = applicationContext.getBean(ComponentService.class);
                     var dryRun = true;
                     // By default, dryRun is true. It is set to false only if the second argument is "realRun".
                     if (args.length > 1 && args[1].equalsIgnoreCase("realRun")) {
                         dryRun = false;
                     }
-                    componentServiceForDelete.deleteComponents(dryRun);
+                    componentService.deleteComponents(dryRun);
                     break;
                 default:
                     System.out.println("Invalid option. Use 'certificate' or '1' for certificates, 'heavyComponents' or '2' for heavyComponents, 'permissions' or '3' for permissions, 'deleteComponents' or '4' for deleteComponents.");
@@ -86,4 +88,5 @@ public class Application implements CommandLineRunner {
             System.out.println("No arguments provided. Use 'certificate' or '1' for certificates, 'heavyComponents' or '2' for heavyComponents, 'permissions' or '3' for permissions, 'deleteComponents' or '4' for deleteComponents. See the README file.");
         }
     }
+
 }
